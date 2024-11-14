0359a82c406a4e4295b5dd709d1f097e7c0fbbcc
Tapashlo <adhikarisandeep321@gmail.com>
2024-11-14 14:13:33 -0600

    Created an About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-lime-500'>About</h1>
+        <p>
+            Welcome to our Game database website! My name is <span className='font-bold text-lime-500'>Adrian</span>. I am very passionate about video games. This website might be your 
+            one stop for all your game needs.
+        </p>
+        <p>
+            Our Website is a comprehensive database of video games from all around the world, along with the latest news, reviews, and trailers. Our database is constantly updated with new releases,
+            making sure you have access to latest and greatest games. You can search the games by name, genre or platform. 
+        </p>
+    </div>
+  )
+}
